e59d4e72892315ca4a641a1a39bb9dd691aece9e
Michael Ball <michael.h.s.ball@gmail.com>
2022-12-02 07:56:21 +0100

    zoom

--- hanga/image.py
+++ hanga/image.py
@@ -114,3 +114,49 @@ def image_grid(imgs: List[np.ndarray], rows: int, cols: int) -> PIL.Image:
     for i, img in enumerate(imgs):
         grid.paste(img, box=(i % cols * w, i // cols * h))
     return grid
+
+
+def cv2_clipped_zoom(img: np.ndarray, zoom_factor: float = 0) -> np.ndarray:
+    """Center zoom in/out of the given image and returning an enlarged/shrinked view of
+    the image without changing dimensions
+
+    Taken from: (https://stackoverflow.com/a/48097478)
+
+    Args:
+        img: numpy array image
+        zoom_factor : amount of zoom as a ratio [0 to Inf). Default 0.
+
+    Returns:
+        result: array of the same shape of the input img zoomed by the specified factor.
+    """
+    if zoom_factor == 0:
+        return img
+
+    height, width = img.shape[:2]  # It's also the final desired shape
+    new_height, new_width = int(height * zoom_factor), int(width * zoom_factor)
+
+    print(width, height)
+    print(new_width, new_height)
+
+    ### Crop only the part that will remain in the result (more efficient)
+    # Centered bbox of the final desired size in resized (larger/smaller) image coordinates
+    y1, x1 = max(0, new_height - height) // 2, max(0, new_width - width) // 2
+    # TODO: is this the midpoint that we can change ??
+    y2, x2 = y1 + height, x1 + width
+    bbox = np.array([y1, x1, y2, x2])
+    # Map back to original image coordinates
+    bbox = (bbox / zoom_factor).astype(int)
+    y1, x1, y2, x2 = bbox
+    cropped_img = img[y1:y2, x1:x2]
+    print((x1, y1), (x2, y2))
+
+    # Handle padding when downscaling
+    resize_height, resize_width = min(new_height, height), min(new_width, width)
+    pad_height1, pad_width1 = (height - resize_height) // 2, (width - resize_width) // 2
+    pad_height2, pad_width2 = (height - resize_height) - pad_height1, (width - resize_width) - pad_width1
+    pad_spec = [(pad_height1, pad_height2), (pad_width1, pad_width2)] + [(0, 0)] * (img.ndim - 2)
+
+    result = cv2.resize(cropped_img, (resize_width, resize_height))
+    result = np.pad(result, pad_spec, mode="constant")
+    assert result.shape[0] == height and result.shape[1] == width
+    return result
